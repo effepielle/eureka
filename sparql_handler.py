bdf120bc30de47406c7b6d35aea619727f55dcb2
frMitola <61455943+frMitola@users.noreply.github.com>
2022-11-27 21:46:43 +0100

    add some functions and corrections

--- sparql_handler.py
+++ sparql_handler.py
@@ -1,69 +1,40 @@
+import numpy as np
 from SPARQLWrapper import SPARQLWrapper, JSON
 import pandas as pd
 
 sparql = SPARQLWrapper("https://query.wikidata.org/bigdata/namespace/wdq/sparql")
 
-# WikiData Item Name - WikiData Item ID
-dict = {
-    "park": "Q22698",
-    "public_garden": "Q55177716",
-    "city_walls": "Q16748868",
-    "church_building": "Q16970"
-    #TODO: add more items
-}
-
-for site_name, site_wikidata_id in dict.items():
-    if site_name == "city_walls":
-        sparql.setQuery("""
-    SELECT ?site ?siteLabel ?siteAccessibilityLabel ?siteTripAdvisorIdLabel ?siteImage{
-    {
-    SELECT DISTINCT ?site ?siteLabel ?siteAccessibilityLabel ?siteTripAdvisorIdLabel ?siteImage
-    WHERE
-    {       
-    ?site wdt:P131 wd:Q13375;
-            wdt:P31/wdt:P279* wd:""" + site_wikidata_id + """;
-    OPTIONAL {?site wdt:P2846 ?siteAccessibility.}
-    OPTIONAL {?site wdt:P3134 ?siteTripAdvisorId.}
-    OPTIONAL {?site wdt:P18 ?siteImage.}
-
-    SERVICE wikibase:label { bd:serviceParam wikibase:language "en"}.
+def main():
+    # WikiData Item Name - WikiData Item ID
+    dict = {
+        "park": "Q22698",
+        "public_garden": "Q55177716",
+        "city_walls": "Q16748868",
+        "church_building": "Q16970"
+        #TODO: add more items
     }
-    }
-    FILTER (!REGEX(?siteLabel, "^Q[0-9]+$"))
-    }
-    """)
-        sparql.setReturnFormat(JSON)
-        results = sparql.query().convert()
 
-        results_df = pd.json_normalize(results['results']['bindings'])
+    for site_name, site_wikidata_id in dict.items():
+        if site_name == "city_walls":
+            compute_hard_coded_address_query(site_name,site_wikidata_id, 43.72518845930142, 10.39367112698515)
+        else:
+            compute_query(site_name,site_wikidata_id)
 
-        #hard coded coordinates of city walls from Piazza dei Miracoli (source: Google Maps)
-        for index, row in results_df.iterrows():
-            predicate_string = "{}({}, {}, {}, {} ".format(site_name, row["site.value"].split('http://www.wikidata.org/entity/')[1], row["siteLabel.value"], "43.72518845930142", "43.72518845930142") 
-            
-            if 'siteAccessibilityLabel.value' in results_df:
-                predicate_string += "{}, ".format(row["siteAccessibilityLabel.value"])
-            if 'siteTripAdvisorId.value' in results_df:
-                predicate_string += "{}, ".format(row["siteTripAdvisorId.value"])
-            if 'siteImage.value' in results_df:
-                predicate_string += "{}".format(row["siteImage.value"])
-            predicate_string += ").\n"
 
-    else:
-        #TODO: define a function
-        sparql.setQuery("""
+def compute_query(site_name,wikidata_id):
+    sparql.setQuery("""
     SELECT ?site ?siteLabel ?siteLat ?siteLon ?siteAccessibilityLabel ?siteTripAdvisorIdLabel ?siteImage{
     {
     SELECT DISTINCT ?site ?siteLabel ?siteLat ?siteLon ?siteAccessibilityLabel ?siteTripAdvisorIdLabel ?siteImage
     WHERE
     {       
     ?site wdt:P131 wd:Q13375;
-            wdt:P31/wdt:P279* wd:""" + site_wikidata_id + """;
+            wdt:P31/wdt:P279* wd:""" + wikidata_id + """;
             p:P625 ?siteCoordinates;
     OPTIONAL {?site wdt:P2846 ?siteAccessibility.}
     OPTIONAL {?site wdt:P3134 ?siteTripAdvisorId.}
     OPTIONAL {?site wdt:P18 ?siteImage.}
-    
+
     ?siteCoordinates psv:P625 ?coordinate_node.
     ?coordinate_node wikibase:geoLongitude ?siteLon.
     ?coordinate_node wikibase:geoLatitude ?siteLat.
@@ -73,22 +44,74 @@ for site_name, site_wikidata_id in dict.items():
     FILTER (!REGEX(?siteLabel, "^Q[0-9]+$"))
     }
     """)
-    
-        sparql.setReturnFormat(JSON)
-        results = sparql.query().convert()
 
-        results_df = pd.json_normalize(results['results']['bindings'])
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
 
-        for index, row in results_df.iterrows():
-            predicate_string = "{}({}, {}, {}, {}, ".format(site_name, row["site.value"].split('http://www.wikidata.org/entity/')[1], row["siteLabel.value"], row["siteLat.value"], row["siteLon.value"])
-            if 'siteAccessibilityLabel.value' in results_df:
+    results_df = pd.json_normalize(results['results']['bindings'])
+
+    f = open("KB.pl", 'a+')
+    for index, row in results_df.iterrows():
+        predicate_string = "{}(\"{}\", \"{}\", {}, {}, ".format(site_name,
+                                                        row["site.value"].split('http://www.wikidata.org/entity/')[1],
+                                                        row["siteLabel.value"], row["siteLat.value"],
+                                                        row["siteLon.value"])
+        if 'siteAccessibilityLabel.value' in results_df:
+            if pd.notna(row["siteAccessibilityLabel.value"]):
+                predicate_string += "\"{}\", ".format(row["siteAccessibilityLabel.value"])
+            else:
                 predicate_string += "{}, ".format(row["siteAccessibilityLabel.value"])
-            if 'siteTripAdvisorId.value' in results_df:
-                predicate_string += "{}, ".format(row["siteTripAdvisorId.value"])
-            if 'siteImage.value' in results_df:
-                predicate_string += "{}".format(row["siteImage.value"])
-            predicate_string += ").\n"
+        if 'siteTripAdvisorId.value' in results_df:
+            predicate_string += "{}, ".format(row["siteTripAdvisorId.value"])
+        if 'siteImage.value' in results_df:
+            predicate_string += "\"{}\"".format(row["siteImage.value"])
+        predicate_string += ").\n"
+        f.write(predicate_string)
+    f.close()
 
-#TODO: save items into a KB file
+def compute_hard_coded_address_query(site_name,wikidata_id, lat, lon):
+    sparql.setQuery("""
+        SELECT ?site ?siteLabel ?siteAccessibilityLabel ?siteTripAdvisorIdLabel ?siteImage{
+        {
+        SELECT DISTINCT ?site ?siteLabel ?siteAccessibilityLabel ?siteTripAdvisorIdLabel ?siteImage
+        WHERE
+        {       
+        ?site wdt:P131 wd:Q13375;
+                wdt:P31/wdt:P279* wd:""" + wikidata_id + """;
+        OPTIONAL {?site wdt:P2846 ?siteAccessibility.}
+        OPTIONAL {?site wdt:P3134 ?siteTripAdvisorId.}
+        OPTIONAL {?site wdt:P18 ?siteImage.}
 
+        SERVICE wikibase:label { bd:serviceParam wikibase:language "en"}.
+        }
+        }
+        FILTER (!REGEX(?siteLabel, "^Q[0-9]+$"))
+        }
+        """)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
 
+    results_df = pd.json_normalize(results['results']['bindings'])
+
+    f = open("KB.pl", 'a+')
+    for index, row in results_df.iterrows():
+        # hard-coded coordinates of city walls entrance from Piazza dei Miracoli (source: Google Maps)
+        predicate_string = "{}(\"{}\", \"{}\", {}, {}, ".format(site_name,
+                                                       row["site.value"].split('http://www.wikidata.org/entity/')[1],
+                                                       row["siteLabel.value"], lat, lon)
+
+        if 'siteAccessibilityLabel.value' in results_df:
+            if pd.notna(row["siteAccessibilityLabel.value"]):
+                predicate_string += "\"{}\", ".format(row["siteAccessibilityLabel.value"])
+            else:
+                predicate_string += "{}, ".format(row["siteAccessibilityLabel.value"])
+        if 'siteTripAdvisorId.value' in results_df:
+            predicate_string += "{}, ".format(row["siteTripAdvisorId.value"])
+        if 'siteImage.value' in results_df:
+            predicate_string += "\"{}\"".format(row["siteImage.value"])
+        predicate_string += ").\n"
+        f.write(predicate_string)
+    f.close()
+
+if __name__ == '__main__':
+    main()
