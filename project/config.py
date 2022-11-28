44f2e84e7c8d851a9f67ba5b9435edff5ccce5ff
frMitola <61455943+frMitola@users.noreply.github.com>
2022-11-28 16:46:13 +0100

    add some comment

--- project/config.py
+++ project/config.py
@@ -4,6 +4,7 @@ import os
 DIALOGFLOW_PROJECT_ID = 'eureka-wlon'
 DIALOGFLOW_LANGUAGE_CODE = 'en'
 
+#change needed because cd is in "...\project\io-package" and there is no .json in it
 def costum_abs_path(file):
     os.chdir("..")
     return os.path.abspath(file)
