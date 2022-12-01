ba4899447c05b395c86b96989ee79e514b423e06
Francesco Paolo Liuzzi <f.liuzzi@studenti.unipi.it>
2022-12-01 20:03:48 +0100

    fix prolog warnings

--- rules.pl
+++ rules.pl
@@ -1,7 +1,7 @@
 indoor(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star) :- church_building(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star);
                                                        museum(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star).
 
-outdoor(Id,Label,Lat,Lon,Accessibility,TripId,Image, _) :- park(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star);
+outdoor(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star) :- park(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star);
                                                         public_garden(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star);
                                                         city_walls(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star);
                                                         monument(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star).
@@ -9,6 +9,6 @@ outdoor(Id,Label,Lat,Lon,Accessibility,TripId,Image, _) :- park(Id,Label,Lat,Lon
 wheelchair_accessible(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star) :- Accessibility = "wheelchair accessible", (indoor(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star); 
                                                                                                                 outdoor(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star)).
 
-visitable_if_raining(Id,Label,Lat,Lon,Accessibility,TripId,Image, Stars) :- indoor(Id,Label,Lat,Lon,Accessibility,TripId,Image , Star).
+visitable_if_raining(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star) :- indoor(Id,Label,Lat,Lon,Accessibility,TripId,Image , Star).
 
 best_site(Id,Label,Lat,Lon,Accessibility,TripId,Image,Star) :- (indoor(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star); outdoor(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star)), Star >=4.
