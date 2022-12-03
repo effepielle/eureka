20f1eb0c8879c60c7d12ac196d95a92d270d3ec2
Francesco Paolo Liuzzi <f.liuzzi@studenti.unipi.it>
2022-12-03 19:18:53 +0100

    add telegram start command handler

--- project/io-package/io_handler.py
+++ project/io-package/io_handler.py
@@ -2,32 +2,26 @@ import sys
 
 sys.path.append('../eureka')
 import telebot
-from project.dialogflow_api import nlp_handler as nlp
+from telebot import types
 from project.config_files.config import TELEGRAM_TOKEN
 
 # for testing: https://t.me/eurekachatbot
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 
+sites_list = [["Parks", "Public Gardens", "City Walls"], ["Churches"]] #TODO: add more items
 
-'''
-boilerplate for default command handling
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.send_message(message.chat.id, "Hello world")
-'''
+    bot.send_message(message.chat.id, "Hello {}! I'm {} and I can help you to discover cultural assets in Pisa.".format(message.chat.first_name, "EUREKA"))
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(types.KeyboardButton("Parks"), types.KeyboardButton("Public Garden"), types.KeyboardButton("City Walls"))
+    keyboard.add(types.KeyboardButton("Churches"))
+    bot.send_message(message.chat.id, "What would you like to visit?", reply_markup=keyboard)
 
 #text handling
 @bot.message_handler(content_types=['text'])
-def handle_NL_text(message):
-    # TODO:
-
-    # if we want to pass it to df module
-     out = nlp.analyze_input(message.chat.id, message.text)
-     bot.send_message(message.chat.id, out)
-
-    # else we want to process locally message without df module NLP processing
-    # out = process_costum_input(session_id, message)
-    # bot.send_message(message.chat.id, out)
+def text_handling(message):
+    pass
 
 
 bot.infinity_polling()
