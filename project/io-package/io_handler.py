22730a06d28c821b1742a354cc58e13375ade3e5
frMitola <61455943+frMitola@users.noreply.github.com>
2022-12-04 09:41:52 +0100

    create a basic type of interaction

--- project/io-package/io_handler.py
+++ project/io-package/io_handler.py
@@ -1,22 +1,26 @@
 import sys
-
 sys.path.append('../eureka')
 import telebot
 from telebot import types
 from project.config_files.config import TELEGRAM_TOKEN
+from utilities import *
 
 # for testing: https://t.me/eurekachatbot
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 
-sites_list = [["Parks", "Public Gardens", "City Walls"], ["Churches"]] #TODO: add more items
 
+
+# conversation flow
 @bot.message_handler(commands=['start'])
-def send_welcome(message):
+def handle_conversation(message):
     bot.send_message(message.chat.id, "Hello {}! I'm {} and I can help you to discover cultural assets in Pisa.".format(message.chat.first_name, "EUREKA"))
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(types.KeyboardButton("Parks"), types.KeyboardButton("Public Garden"), types.KeyboardButton("City Walls"))
-    keyboard.add(types.KeyboardButton("Churches"))
-    bot.send_message(message.chat.id, "What would you like to visit?", reply_markup=keyboard)
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    bot.send_message(message.chat.id, "What would you like to do?", reply_markup=keyboard)
+    bot.register_next_step_handler(message, create_keyboard(keyboard))
+    bot.register_next_step_handler(message, greetings)
+
+def greetings(message):
+    bot.send_message(message.chat.id, "Ok, bye")
 
 #text handling
 @bot.message_handler(content_types=['text'])
@@ -24,4 +28,6 @@ def text_handling(message):
     pass
 
 
+#bot.enable_save_next_step_handlers(delay=1)
+#bot.load_next_step_handlers()
 bot.infinity_polling()
