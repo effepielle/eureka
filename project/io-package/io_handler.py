242a817475917cc56b502ef6537084ab624f8432
Francesco Paolo Liuzzi <f.liuzzi@studenti.unipi.it>
2022-12-04 11:04:58 +0100

    fix next step handler for user's asset type choice

--- project/io-package/io_handler.py
+++ project/io-package/io_handler.py
@@ -1,33 +1,30 @@
 import sys
-sys.path.append('../eureka')
+import os
+import re
+
+subdir = re.sub("eureka.*","eureka",os.getcwd())
+os.chdir(subdir)
+sys.path.append('../eureka/project')
+
 import telebot
 from telebot import types
-from project.config_files.config import TELEGRAM_TOKEN
-from utilities import *
+#from project.config_files.config import TELEGRAM_TOKEN
+from backend.utilities import *
 
 # for testing: https://t.me/eurekachatbot
-bot = telebot.TeleBot(TELEGRAM_TOKEN)
-
-
-
-# conversation flow
+bot = telebot.TeleBot('5689759624:AAHfrWL9xPd8KlG11ah1wm95EMago4TK6AI') #TODO: fix token path
+ 
+ #STEP 0: bot is started and asks user to choose an asset type (churches, monuments, towers, etc.)
 @bot.message_handler(commands=['start'])
 def handle_conversation(message):
     bot.send_message(message.chat.id, "Hello {}! I'm {} and I can help you to discover cultural assets in Pisa.".format(message.chat.first_name, "EUREKA"))
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-    bot.send_message(message.chat.id, "What would you like to do?", reply_markup=keyboard)
-    bot.register_next_step_handler(message, create_keyboard(keyboard))
-    bot.register_next_step_handler(message, greetings)
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True) 
+    create_keyboard(keyboard)
+    msg = bot.send_message(message.chat.id, "What would you like to do?", reply_markup=keyboard) #TODO: fix question
+    bot.register_next_step_handler(msg, site_label_handler)
 
-def greetings(message):
-    bot.send_message(message.chat.id, "Ok, bye")
+# STEP 1: the user choice is handled
+def site_label_handler(message):
+    print(message.text)
 
-#text handling
-@bot.message_handler(content_types=['text'])
-def text_handling(message):
-    pass
-
-
-#bot.enable_save_next_step_handlers(delay=1)
-#bot.load_next_step_handlers()
 bot.infinity_polling()
