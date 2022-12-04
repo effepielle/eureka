6051b998a4720306a68e8e87cfba25e1b9eb674d
Francesco Paolo Liuzzi <f.liuzzi@studenti.unipi.it>
2022-12-04 10:43:53 +0100

    delete dialogflow refs code and files

--- project/config-files/config.py
+++ project/config-files/config.py
@@ -8,13 +8,6 @@ def custom_abs_path(file):
     os.chdir(subdir)
     return os.path.abspath(file)
 
-#### DIALOGFLOW ###
-
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = custom_abs_path("eureka-wlon-fb3248e88a69.json")
-DIALOGFLOW_PROJECT_ID = 'eureka-wlon'
-DIALOGFLOW_LANGUAGE_CODE = 'en'
-
-#### DIALOGFLOW ###
 TELEGRAM_TOKEN = '5689759624:AAHfrWL9xPd8KlG11ah1wm95EMago4TK6AI'
 
 
