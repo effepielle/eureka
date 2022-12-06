41bb66803d8213e00e6141dcfeb11aee7569ba39
frMitola <61455943+frMitola@users.noreply.github.com>
2022-12-06 11:22:44 +0100

    add some rules to KB

--- project/kb_configuration/rules.pl
+++ project/kb_configuration/rules.pl
@@ -1,15 +1,23 @@
+%TODO add more categories
+    %partition by categories
+arts_and_culture(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star) :- museum(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star).
+architecture(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star) :- city_walls(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star); church_building(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star);
+                                                                    square(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star);monument(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star).
+green_areas(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star) :- park(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star); public_garden(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star).
+
 indoor(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star) :- church_building(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star);
                                                        museum(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star).
 
-outdoor(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star) :- park(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star);
-                                                        public_garden(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star);
-                                                        city_walls(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star);
-                                                        monument(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star).
+outdoor(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star) :- green_areas(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star); city_walls(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star);
+                                                        monument(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star);square(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star).
 
-wheelchair_accessible(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star) :- Accessibility = "wheelchair accessible", (indoor(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star); 
-                                                                                                                outdoor(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star)).
+%wheelchair_accessible(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star) :- Accessibility = "wheelchair accessible", (arts_and_culture(Id,Label,Lat,Lon,Accessibility,TripId,Image, Site_star);
+%                                                                                                                architecture(Id,Label,Lat,Lon,Accessibility,TripId,Image, Site_star); green_areas(Id,Label,Lat,Lon,Accessibility,TripId,Image, Site_star)).
 
 visitable_if_raining(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star) :- indoor(Id,Label,Lat,Lon,Accessibility,TripId,Image , Star).
 
-    %not interested in other information while querying for best cultural site
-best_site(Id,Label,Lat,Lon,Accessibility,TripId,Image,Star) :- (indoor(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star); outdoor(Id,Label,Lat,Lon,Accessibility,TripId,Image, Star)), Star >=4.
+%variable threshold settable by user
+best_site(Id,Label,Lat,Lon,Accessibility,TripId,Image,Star) :- arts_and_culture(Id,Label,Lat,Lon,Accessibility,TripId,Image, Site_star), Site_star >= Star.
+best_site(Id,Label,Lat,Lon,Accessibility,TripId,Image,Star) :- architecture(Id,Label,Lat,Lon,Accessibility,TripId,Image, Site_star) , Site_star >= Star.
+best_site(Id,Label,Lat,Lon,Accessibility,TripId,Image,Star) :-  green_areas(Id,Label,Lat,Lon,Accessibility,TripId,Image, Site_star), Site_star >= Star.
+    %same as best_site will be also the cost (imposing a threshold)
